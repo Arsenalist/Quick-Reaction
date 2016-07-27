d5802b55cd75b487e2eb04a1cce306a0ef0c9c88
Zarar Siddiqi <zarar_siddiqi@epam.com>
2016-07-26 23:34:17 -0400

    Add index page

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 var Route = ReactRouter.Route;
+var Main = require('./components/Main.react.js');
 var MLBReaction = require('./components/MLBReaction.react.js');
 var MLSReaction = require('./components/MLSReaction.react.js');
 var MLSPreview = require('./components/MLSPreview.react.js');
@@ -9,7 +10,8 @@ var App = require('./components/App.react.js');
 // declare our routes and their hierarchy
 var routes = (
   <Route handler={App}>
-    <Route path="/" handler={MLBReaction}/>
+    <Route path="/" handler={Main}/>
+    <Route path="/baseball/reaction" handler={MLBReaction}/>
     <Route path="/soccer/reaction" handler={MLSReaction}/>
     <Route path="/soccer/preview" handler={MLSPreview}/>
   </Route>
---
+++ scripts/components/Main.react.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var Link = require('react-router').Link;
+
+var Main = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <div className="list-group">
+          <Link to="/baseball/reaction" className="list-group-item">
+            <h4 className="list-group-item-heading">Baseball Reaction</h4>
+          </Link>
+          <Link to="/soccer/reaction" className="list-group-item">
+            <h4 className="list-group-item-heading">Soccer Reaction</h4>
+          </Link>
+          <Link to="/soccer/preview" className="list-group-item">
+            <h4 className="list-group-item-heading">Soccer Preview</h4>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+});
+module.exports = Main;
